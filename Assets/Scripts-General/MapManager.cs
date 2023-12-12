290c7a30da65306bdb52038d7574e8d70cdc8818
Komet07 <simonlouis@bluewin.ch>
2023-12-12 21:08:36 +0100

    Functional Save / Load system

--- Assets/Scripts-General/MapManager.cs
+++ Assets/Scripts-General/MapManager.cs
@@ -1,14 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Xml;
+using System.Xml.Serialization;
+using System.IO;
 
 [System.Serializable]
 public class Sector
 {
+    [XmlAttribute("name")]
     public string _name = "";
+    [XmlAttribute("control")]
     public int _controlFaction = -1;
     public int _posXInt = 0;
     public int _posYInt = 0;
+}
+
+[System.Serializable]
+[XmlRoot("MapObject")]
+public class Map
+{
+    [XmlArray("Sectors"), XmlArrayItem("Sector")]
+    public List<Sector> _sectors = new List<Sector>() { };
+
+    public static Map Load(string path)
+    {
+        var serializer = new XmlSerializer(typeof(Map));
+        using (var stream = new FileStream(path, FileMode.Open))
+        {
+            return serializer.Deserialize(stream) as Map;
+        }
+    }
 }
 
 public class MapManager : MonoBehaviour
@@ -22,7 +44,7 @@ public class MapManager : MonoBehaviour
     }
     #endregion
 
-    public List<Sector> _sectors = new List<Sector>() { };
+    public Map _map = new Map();
 
 
     // Start is called before the first frame update
---
+++ Assets/Scripts-General/XMLWriter.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Xml;
+using System.Xml.Serialization;
+using System.IO;
+
+public class XMLWriter : MonoBehaviour
+{
+
+    public bool _save = false;
+    public bool _load = false;
+    public string _saveFileName = "save1";
+
+    void Save()
+    {
+
+        var serializer = new XmlSerializer(typeof(Map));
+        using (var stream = new FileStream(Path.Combine(Application.dataPath, "saves/"+_saveFileName+".xml"), FileMode.Create))
+        {
+            serializer.Serialize(stream, MapManager.Instance._map);
+            Debug.Log(Path.Combine(Application.dataPath, "saves/" + _saveFileName + ".xml"));
+        }
+    }
+
+    
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_save)
+        {
+            _save = false;
+            Save();
+        }
+
+        if (_load)
+        {
+            _load = false;
+            var _map = Map.Load(Path.Combine(Application.dataPath, "saves/"+_saveFileName+".xml"));
+            MapManager.Instance._map = _map;
+        }
+    }
+}
